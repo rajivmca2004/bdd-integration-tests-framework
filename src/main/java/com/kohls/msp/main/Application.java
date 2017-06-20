115ef1b7ca611149eabbf39de1d25ca39f7b3887
Rajiv Srivastava <rajiv.srivastava@kohls.com>
2017-06-21 00:04:54 +0530

    [MRP-2741] Moved property config to Application class

--- src/main/java/com/kohls/msp/main/Application.java
+++ src/main/java/com/kohls/msp/main/Application.java
@@ -2,6 +2,13 @@ package com.kohls.msp.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import com.kohls.msp.common.BddEnum;
 
 /**
  * This class contains main Spring Boot class
@@ -10,8 +17,51 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @since 06/01/2017
  */
 @SpringBootApplication
-public class Application{
+public class Application {
+
+	private static final Resource[] DEV_PROPERTIES = new ClassPathResource[] {
+			new ClassPathResource(BddEnum.DEV_ENV.value()) };
+	private static final Resource[] TEST_PROPERTIES = new ClassPathResource[] {
+			new ClassPathResource(BddEnum.TEST_ENV.value()) };
+	private static final Resource[] LOCAL_DEFAULT_PROPERTIES = new ClassPathResource[] {
+			new ClassPathResource(BddEnum.LOCAL_ENV.value()) };
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+	
+	/*
+	 * Environment profiling
+	 */
+
+	@Profile("dev")
+	public static class DevConfig {
+		@Bean
+		public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+			PropertySourcesPlaceholderConfigurer pspc = new PropertySourcesPlaceholderConfigurer();
+			pspc.setLocations(DEV_PROPERTIES);
+			pspc.setIgnoreUnresolvablePlaceholders(true);
+			return pspc;
+		}
+	}
+
+	@Profile("test")
+	public static class TestConfig {
+		@Bean
+		public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+			PropertySourcesPlaceholderConfigurer pspc = new PropertySourcesPlaceholderConfigurer();
+			pspc.setLocations(TEST_PROPERTIES);
+			return pspc;
+		}
+	}
+
+	@Profile("default")
+	public static class ProdConfig {
+		@Bean
+		public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+			PropertySourcesPlaceholderConfigurer pspc = new PropertySourcesPlaceholderConfigurer();
+			pspc.setLocations(LOCAL_DEFAULT_PROPERTIES);
+			return pspc;
+		}
 	}
 }
